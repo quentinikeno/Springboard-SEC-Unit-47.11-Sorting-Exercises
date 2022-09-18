41f6578f09d428ee0a4e223d91ecc06e2dea8c34
Quentin Ikeno <ikenoq@gmail.com>
2022-09-17 18:20:06 -1000

    Write bubbleSort function.

--- bubble.js
+++ bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort() {}
+function bubbleSort(array) {
+	for (let i = 0; i < array.length; i++) {
+		let swapped = false;
+		for (let j = 0; j < array.length - i; j++) {
+			if (array[j] > array[j + 1]) {
+				const temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				swapped = true;
+			}
+		}
+		if (!swapped) break;
+	}
+	return array;
+}
 
 module.exports = bubbleSort;
