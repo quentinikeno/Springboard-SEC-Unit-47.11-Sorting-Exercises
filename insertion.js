5ab1a3af99f5079468a13d1d73976f806cedb5c7
Quentin Ikeno <ikenoq@gmail.com>
2022-09-18 11:44:33 -1000

    Write function for insertionSort.

--- insertion.js
+++ insertion.js
@@ -1,3 +1,24 @@
-function insertionSort() {}
+function insertionSort(array) {
+	const result = array.length ? [array[0]] : [];
+
+	for (let i = 1; i < array.length; i++) {
+		const element = array[i];
+
+		if (element >= result[result.length - 1]) {
+			result.push(element);
+		} else if (element <= result[0]) {
+			result.unshift(element);
+		} else {
+			for (let j = 0; j < result.length; j++) {
+				if (result[j] < element && result[j + 1] >= element) {
+					result.splice(j + 1, 0, element);
+					break;
+				}
+			}
+		}
+	}
+
+	return result;
+}
 
 module.exports = insertionSort;
