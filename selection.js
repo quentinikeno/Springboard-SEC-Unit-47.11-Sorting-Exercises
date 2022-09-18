94c9dd54a2df5e7190a002d251e14afef7936ba4
Quentin Ikeno <ikenoq@gmail.com>
2022-09-18 13:13:28 -1000

    Write function for selectionSort.

--- selection.js
+++ selection.js
@@ -1,3 +1,19 @@
-function selectionSort() {}
+function selectionSort(array) {
+	for (let i = 0; i < array.length; i++) {
+		let minIndex = i;
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] < array[minIndex]) {
+				minIndex = j;
+			}
+		}
+		if (i !== minIndex) {
+			const min = array[minIndex];
+			array[minIndex] = array[i];
+			array[i] = min;
+		}
+	}
+
+	return array;
+}
 
 module.exports = selectionSort;
