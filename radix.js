e77e7c20b4899ba0f15d00427abba4cf0606c5f0
Quentin Ikeno <ikenoq@gmail.com>
2022-09-18 20:20:37 -1000

    Write getdigit, digitCount, and mostDigits functions.

--- radix.js
+++ radix.js
@@ -1,3 +1,20 @@
+function getDigit(num, index) {
+	return Math.floor(num / Math.pow(10, index)) % 10;
+}
+
+function digitCount(num) {
+	return num === 0 ? 1 : Math.floor(Math.log10(num)) + 1;
+}
+
+function mostDigits(arr) {
+	let maxDigits = 0;
+	for (let num of arr) {
+		const digits = digitCount(num);
+		if (digits > maxDigits) maxDigits = digits;
+	}
+	return maxDigits;
+}
+
 function radixSort() {}
 
-module.exports = radixSort;
+module.exports = { getDigit, digitCount, mostDigits, radixSort };
